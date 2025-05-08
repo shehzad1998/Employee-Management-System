e85cf082735e4ce70a95eafd8724f05727ea6224
shehzad1998 <shaikhshehzad26@gmail.com>
2025-05-08 20:18:26 +0530

    Changes for frontend get production ready

--- ems-frontend/vite.config.ts
+++ ems-frontend/vite.config.ts
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          mui: ['@mui/material', '@mui/icons-material'],
+        }
+      }
+    }
+  }
 })
